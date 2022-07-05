9d1ab7c6d30c32345003d7af16304a69fb8d99d9
Arka <arkadatta2002@gmail.com>
2022-07-06 02:57:51 +0530

    "navbar and wallet connection"

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar1 from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar1 />
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+
+      <h1>Hi</h1>
+
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+
+      <h1>Hi</h1>
+
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      
+
+    </div>
+  );
+}
+
+export default App;
